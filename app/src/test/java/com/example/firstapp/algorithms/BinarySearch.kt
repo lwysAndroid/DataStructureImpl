39b2796c960be104d8aa4e88d92717a9b58f4024
Luis Tovar <lwys.tovar.war@gmail.com>
2026-08-13 12:49:43 -0600

    Implement Binary Search Recursively

--- app/src/test/java/com/example/firstapp/algorithms/BinarySearch.kt
+++ app/src/test/java/com/example/firstapp/algorithms/BinarySearch.kt
@@ -56,4 +56,73 @@ class BinarySearch {
         }
         return null
     }
+
+    @Test
+    fun testBinarySearchRecursively() {
+        println("-----------------------------------------------")
+
+        println("BinarySearch Recursively")
+
+        testBinarySearchRecursively(item = 4, data = testArrayEvenSize)
+        testBinarySearchRecursively(item = 4, data = testArrayOddSize)
+        testBinarySearchRecursively(item = 5, data = testArrayOddSize)
+        testBinarySearchRecursively(item = 10, data = testArrayOddSize)
+        testBinarySearchRecursively(item = -2, data = testArrayOddSize)
+        testBinarySearchRecursively(item = 100, data = testArrayOddSize)
+        testBinarySearchRecursively(item = -20, data = testArrayOddSize)
+        testBinarySearchRecursively(item = 4, data = emptyList())
+
+        println("-----------------------------------------------")
+    }
+
+    private fun testBinarySearchRecursively(item: Int, data: List<Int>) {
+        val indexOfItem = binarySearchRecursively(item = item, data = data)
+        println("$data index of $item is: $indexOfItem")
+    }
+
+    private fun binarySearchRecursively(
+        item: Int,
+        data: List<Int>,
+        leftIndexOfTheSearch: Int = 0,
+        rightIndexOfTheSearch: Int = data.size - 1
+    ): Int? {
+
+        if (data.isEmpty()) { // Empty lists are not allowed.
+            return null
+        }
+
+        if (leftIndexOfTheSearch > rightIndexOfTheSearch) { // Termination condition 1 of 2
+            return null
+        } else {
+            val middleIndex = (leftIndexOfTheSearch + rightIndexOfTheSearch) / 2
+            val middleValue = data[middleIndex]
+
+            return when {
+                middleValue == item -> { // Termination condition 2 of 2
+                    middleIndex
+                }
+
+                middleValue > item -> { // Recursive case 1 of 2
+                    val newRightIndexOfTheSearch = middleIndex - 1
+                    binarySearchRecursively(
+                        item = item,
+                        data = data,
+                        leftIndexOfTheSearch = leftIndexOfTheSearch,
+                        rightIndexOfTheSearch = newRightIndexOfTheSearch
+                    )
+                }
+
+                // middleValue < item
+                else -> { // Recursive case 2 of 2
+                    val newLeftIndexOfTheSearch = middleIndex + 1
+                    binarySearchRecursively(
+                        item = item,
+                        data = data,
+                        leftIndexOfTheSearch = newLeftIndexOfTheSearch,
+                        rightIndexOfTheSearch = rightIndexOfTheSearch
+                    )
+                }
+            }
+        }
+    }
 }
