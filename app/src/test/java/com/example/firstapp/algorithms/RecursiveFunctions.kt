977e2f0f5d72f1f4671bf56a93a9966db739ec64
Luis Tovar <lwys.tovar.war@gmail.com>
2026-08-13 11:16:20 -0600

    Implement sum of elements in an Array Recursively

--- app/src/test/java/com/example/firstapp/algorithms/RecursiveFunctions.kt
+++ app/src/test/java/com/example/firstapp/algorithms/RecursiveFunctions.kt
@@ -5,15 +5,9 @@ import org.junit.Test
 class RecursiveFunctions {
 
     @Test
-    fun testRecursiveFunctions() {
+    fun testCountdownRecursiveFunction() {
         println("-----------------------------------------------")
-        /*---- countdown ----*/
-        //countdownRecursiveFunction(start = 8)
-
-        /*---- Factorial ----*/
-        val factorialOf = 5
-        val factorial = factorialRecursiveFunction(factorialOf = factorialOf)
-        println("Result $factorialOf! = $factorial")
+        countdownRecursiveFunction(start = 8)
         println("-----------------------------------------------")
     }
 
@@ -25,6 +19,15 @@ class RecursiveFunctions {
             println("")
             return
         }
+    }
+
+    @Test
+    fun testFactorialRecursiveFunction() {
+        println("-----------------------------------------------")
+        val factorialOf = 5
+        val factorial = factorialRecursiveFunction(factorialOf = factorialOf)
+        println("Result $factorialOf! = $factorial")
+        println("-----------------------------------------------")
     }
 
     private fun factorialRecursiveFunction(factorialOf: Int): Int {
@@ -40,6 +43,37 @@ class RecursiveFunctions {
             val factorial = factorialOf * factorialRecursiveFunction(factorialOf - 1)
             println("pop function of the stack, $factorialOf! = $factorial")
             factorial
+        }
+    }
+
+    @Test
+    fun testSumOfElementsInAnArrayRecursiveFunction() {
+        println("-----------------------------------------------")
+        val nonEmptyArray = arrayListOf(2, 4, 6, 1)
+        val sumNonEmptyArray = sumOfElementsInAnArrayRecursiveFunction(array = nonEmptyArray)
+        println("sumNonEmptyArray of $nonEmptyArray is $sumNonEmptyArray")
+
+        val arrayWithOnElement = arrayListOf(5)
+        val sumArrayWithOnElement =
+            sumOfElementsInAnArrayRecursiveFunction(array = arrayWithOnElement)
+        println("sumArrayWithOnElement of $arrayWithOnElement is $sumArrayWithOnElement")
+
+        val emptyArray = arrayListOf<Int>()
+        val sumEmptyArray = sumOfElementsInAnArrayRecursiveFunction(array = emptyArray)
+        println("sumEmptyArray of $emptyArray is $sumEmptyArray")
+        println("-----------------------------------------------")
+    }
+
+    private fun sumOfElementsInAnArrayRecursiveFunction(array: ArrayList<Int>): Int {
+        if (array.isEmpty()) {
+            return -1
+        }
+        if (array.size == 1) { // Bas case or Termination condition
+            return array[0]
+        } else { // Recursive case
+            val lastElement = array.last()
+            val subArray = ArrayList(array.subList(0, array.size - 1))
+            return lastElement + sumOfElementsInAnArrayRecursiveFunction(subArray)
         }
     }
 
