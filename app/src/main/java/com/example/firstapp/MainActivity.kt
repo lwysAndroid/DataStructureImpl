db64c570c5ff731e979bfd678ffe214443192893
Luis Tovar <lwys.tovar.war@gmail.com>
2026-09-17 20:49:17 -0600

    Practice with Coroutines

--- app/src/main/java/com/example/firstapp/MainActivity.kt
+++ app/src/main/java/com/example/firstapp/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.firstapp
 
 import android.content.res.Configuration
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -16,12 +15,17 @@ import androidx.compose.ui.unit.sp
 import androidx.lifecycle.lifecycleScope
 import com.example.firstapp.ui.theme.FirstAppTheme
 import kotlinx.coroutines.*
-import kotlin.coroutines.coroutineContext
-import kotlin.system.measureTimeMillis
+import kotlin.coroutines.ContinuationInterceptor
+import kotlin.time.Duration.Companion.milliseconds
+import kotlin.time.Duration.Companion.seconds
+import kotlin.time.ExperimentalTime
 
 class MainActivity : ComponentActivity() {
 
     private val TAG = MainActivity::class.java.canonicalName
+
+    private val myOwnScope = CoroutineScope(context = Dispatchers.Default + SupervisorJob())
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -36,70 +40,129 @@ class MainActivity : ComponentActivity() {
             }
         }
 
-        /* Log.d(TAG, "onCreate: ${Thread.currentThread().name}")
-         lifecycleScope.launch {
-             Log.d(TAG, "lifecycleScope.launch: ${Thread.currentThread().name}")
-         }
-         lifecycleScope.launch(Dispatchers.Main) {
-             Log.d(TAG, "Dispatchers.Main ${Thread.currentThread().name}")
-         }*/
-
-        /* lifecycleScope.launch {
-             val time = measureTimeMillis {
-                 val response1 = async { network1() }
-                 val response2 = async { network2() }
-                 Log.d(TAG, "response1: ${response1.await()}")
-                 Log.d(TAG, "response2: ${response2.await()}")
-             }
-             Log.d(TAG, "Total tme: ${time}")
-
-         }*/
-
-        lifecycleScope.launch(Dispatchers.IO) {
-            val time = measureTimeMillis {
+        /*with(lifecycleScope) {
+            (0..10).forEach {
                 launch {
-                    delay(15000)
+                    firstSuspendFunction(name = "$it")
                 }
-                val response1 = network1()
-                val response2 = network2()
-                Log.d(TAG, "response1: ${response1}")
-                Log.d(TAG, "response2: ${response2}")
             }
-            Log.d(TAG, "Total tme: ${time}")
+        }*/
 
-        }
+        /*lifecycleScope.launch {
+            coroutineSubtree()
+        }*/
 
-
+        /*lifecycleScope.launch {
+            performBackgroundWork()
+        }*/
+        /*lifecycleScope.launch {
+            testAsyncBuilder()
+        }*/
+        normalFunctionCallingSuspendFunctions(coroutineScope = lifecycleScope)
     }
 }
 
-
-suspend fun network1(): String = coroutineScope {
-    delay(2000)
-    "Response 1"
+private fun normalFunctionCallingSuspendFunctions(coroutineScope: CoroutineScope) {
+    coroutineScope.launch(context = Dispatchers.Main) {
+        testAsyncBuilder()
+        val dispatcher = coroutineContext[ContinuationInterceptor]
+        println("Called from a no suspend function, Current Dispatcher: $dispatcher")
+    }
 }
 
-suspend fun network2(): String {
-    var someVar: String
-    coroutineScope {
-        /*
-         it is launch in the same coroutineScope so the code runs sequentially
-         */
-        delay(1000)
-        someVar = "Response 3"
-        launch {
-            delay(1000)
-            "Response 5"
-        }
-    }
-    CoroutineScope(coroutineContext).launch {/*
-    it is launch in a new CoroutineScope so the code runs independently
+@OptIn(ExperimentalTime::class)
+private suspend fun testAsyncBuilder() {
+    delay(duration = 2.seconds)
+    /*
+    * Once a coroutine is launched, its context (including its dispatcher,
+    * Job, and name) is fixed for its entire lifecycle—unless that specific
+    * coroutine explicitly changes its own context internally using withContext(...).
     */
-        delay(13000)
-        "Response 4"
+    coroutineScope {
+        launch(context = Dispatchers.Default) {
+            (0..5).forEach {
+                var dispatcher = coroutineContext[ContinuationInterceptor]
+                if (it > 3) {
+                    withContext(context = Dispatchers.Main) {
+                        dispatcher = coroutineContext[ContinuationInterceptor]
+                        println("seconds: $it, Current Dispatcher: $dispatcher")
+
+                    }
+                } else {
+                    println("seconds: $it, Current Dispatcher: $dispatcher")
+                }
+                delay(duration = 1.seconds)
+            }
+        }
+        val firstTask = async(context = Dispatchers.IO) {
+            val dispatcher = coroutineContext[ContinuationInterceptor]
+            delay(duration = 2.seconds)
+            "Value of the first task, Current Dispatcher: $dispatcher"
+        }
+        val secondTask = async(context = Dispatchers.Main) {
+            delay(duration = 2.seconds)
+            val dispatcher = coroutineContext[ContinuationInterceptor]
+            println("Current Dispatcher: $dispatcher")
+            145.2F
+        }
+
+        // Awaits both results and use them
+        val firstTaskValue = firstTask.await()
+        val secondTaskValue = secondTask.await()
+
+        println("$firstTaskValue. secondTaskValue: $secondTaskValue")
+
     }
-    delay(2000)
-    return "Response 2"
+}
+
+@OptIn(ExperimentalTime::class)
+private suspend fun performBackgroundWork() {
+    delay(duration = 2.seconds)
+    /*    Use CoroutineScope.launch() to run a task alongside other work when the result
+    isn't needed, or you don't want to wait for it:*/
+    coroutineScope {
+        // Starts a coroutine that runs without blocking the scope
+        val firstJob = launch {
+            // Suspends to simulate background work
+            delay(100.milliseconds)
+            println("Sending notification in background")
+        }
+        launch { }
+        // Main coroutine continues while a previous one suspends
+        println("Scope continues, firstJob.isActive: ${firstJob.isActive}")
+    }
+}
+
+suspend fun firstSuspendFunction(name: String) {
+    withContext(Dispatchers.Default) {
+        (0..20).forEach {
+            println("$it, $name, ${Thread.currentThread().name}")
+            delay(timeMillis = 500)
+        }
+    }
+
+}
+
+@OptIn(ExperimentalTime::class)
+private suspend fun coroutineSubtree() {
+    delay(duration = 2.seconds)
+    // Root of the coroutine subtree
+    coroutineScope { // this: CoroutineScope
+        this.launch {
+            this.launch {
+                delay(duration = 2.seconds)
+                println("Child of the enclosing coroutine completed")
+            }
+            println("Child coroutine 1 completed")
+        }
+
+        this.launch {
+            delay(1.seconds)
+            println("Child coroutine 2 completed")
+        }
+        // Runs only after all children in the coroutineScope have completed
+        println("Coroutine scope completed")
+    }
 }
 
 @Composable
